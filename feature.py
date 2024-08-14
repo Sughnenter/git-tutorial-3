6e23363c35609b70c9b38dc4c54ad517df776350
Shammah Johnson <shamaj2006@gmail.com>
2024-08-14 09:17:20 +0100

    Version 1

---
+++ feature.py
@@ -0,0 +1,10 @@
+features = ("Beautiful", "slim", "Tall", "Short", "Fat")
+for feature in features:
+    if features.index == 3:
+        print 
+        features.index(feature)
+    else:
+      print ("What the fuck is this code")
+
+
+  
