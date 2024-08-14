ed5c74dd74744e3462806cfeac9503b920018f5d
Shammah Johnson <shamaj2006@gmail.com>
2024-08-14 10:13:21 +0100

    Conflict2

--- feature.py
+++ feature.py
@@ -3,7 +3,7 @@ features = ("Beautiful", "slim", "Tall", "Short", "Fat")
 
 features2 = ("Ugly", "Chubby", "Average", )      
 
-features3 = ("smart", "rich")
+conflict2 = ("smart", "rich")
 
 for feature in features:
     if features.index == 3:
